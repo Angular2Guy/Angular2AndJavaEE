7337c3d9ae92aaff48476e21ecbd73f6b99c37d0
Your Name <you@example.com>
2016-12-26 15:25:10 +0100

    Add Docker support in Maven

--- carrental-web/src/main/angular2/carrental/src/app/crrest.service.ts
+++ carrental-web/src/main/angular2/carrental/src/app/crrest.service.ts
@@ -14,10 +14,10 @@
    limitations under the License.
  */
 import { Injectable } from '@angular/core';
-import {Http, Response, RequestOptionsArgs, Headers} from '@angular/http';
-import {PlatformLocation} from '@angular/common';
-import {CrTableRow, CrDetail} from './crTypes';
-import {Observable}     from 'rxjs/Observable';
+import { Http, Response, RequestOptionsArgs, Headers } from '@angular/http';
+import { PlatformLocation } from '@angular/common';
+import { CrTableRow, CrDetail } from './crTypes';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
@@ -26,51 +26,67 @@ import { environment } from '../environments/environment';
 
 @Injectable()
 export class CrRestService {
-                             
-  private _crTableUrlProd = '/rest/model/crTable/mietNr/{mietNr}';  // URL to web api
-  private _crDetailUrlProd = '/rest/model/crDetail/mietNr/{mietNr}/jahr/{jahr}';
-  private _crTableUrlDev = 'http://localhost:8080/carrental-web/rest/model/crTable/mietNr/{mietNr}';  // URL to web api
-  private _crDetailUrlDev = 'http://localhost:8080/carrental-web/rest/model/crDetail/mietNr/{mietNr}/jahr/{jahr}';    
-  private _reqOptionsArgs: RequestOptionsArgs = {headers: new Headers()};
-    
-  constructor(private http: Http, private pl: PlatformLocation) {
-    this._reqOptionsArgs.headers.set('Content-Type', 'application/json');       
-  }
-    
-  getCrTableRows(policeNr: string) {
-    let url = environment.production ? this.pl.getBaseHrefFromDOM() + this._crTableUrlProd : this._crTableUrlDev;
-//    console.log(url);
-    let start = url.indexOf("/crlist/");
-    let end = url.indexOf("/rest/");
-    url = url.substring(0, start) + url.substring(end, url.length);
-    url = url.replace("{mietNr}",policeNr);      
-    return this.http.get(url, this._reqOptionsArgs).map(res => this.unpackTableResponse(res)).catch(this.handleError);    
-  }
-   
-  getCrDetail(policeNr: string, jahr: string) {
-    let url = environment.production ? this.pl.getBaseHrefFromDOM() + this._crDetailUrlProd : this._crDetailUrlDev;
-//    console.log(url);
-    let start = url.indexOf("/crdetail/");
-    let end = url.indexOf("/rest/");
-    url = url.substring(0, start) + url.substring(end, url.length);
-    url = url.replace("{mietNr}", policeNr).replace("{jahr}", jahr);
-    return this.http.get(url, this._reqOptionsArgs).map(res => this.unpackDetailResponse(res)).catch(this.handleError);
-  }
-    
-  private unpackTableResponse(res: Response): CrTableRow[] {
-    //console.log(res);     
-    return <CrTableRow[]>res.json();
-  }
-  
-  private unpackDetailResponse(res: Response): CrDetail {
-      //console.log(res);     
-    return <CrDetail>res.json();
-  }
-    
-  private handleError(error: Response) {
-    // in a real world app, we may send the error to some remote logging infrastructure
-    // instead of just logging it to the console
-    console.error(JSON.stringify(error));  
-    return Observable.throw(error);
-  }
+
+    private _crTableUrlProd = '/rest/model/crTable/mietNr/{mietNr}';  // URL to web api
+    private _crDetailUrlProd = '/rest/model/crDetail/mietNr/{mietNr}/jahr/{jahr}';
+    private _crTableUrlDev = 'http://localhost:8080/carrental-web/rest/model/crTable/mietNr/{mietNr}';  // URL to web api
+    private _crDetailUrlDev = 'http://localhost:8080/carrental-web/rest/model/crDetail/mietNr/{mietNr}/jahr/{jahr}';
+    private _reqOptionsArgs: RequestOptionsArgs = { headers: new Headers() };
+
+    constructor( private http: Http, private pl: PlatformLocation ) {
+        this._reqOptionsArgs.headers.set( 'Content-Type', 'application/json' );
+    }
+
+    getCrTableRows( policeNr: string ) {
+        let url = environment.production ? this.pl.getBaseHrefFromDOM() + this._crTableUrlProd : this._crTableUrlDev;
+        console.log( url );
+        if ( environment.production ) {
+            let start = url.indexOf( "/crlist/" );
+            console.log( "start=" + start );
+            if ( start > -1 ) {
+                let end = url.indexOf( "/rest/" );
+                url = url.substring( 0, start ) + url.substring( end, url.length );
+            } else {
+                url = this._crTableUrlProd.substring( 1 );
+                console.log( url );
+            }
+        }
+        url = url.replace( "{mietNr}", policeNr );
+        return this.http.get( url, this._reqOptionsArgs ).map( res => this.unpackTableResponse( res ) ).catch( this.handleError );
+    }
+
+    getCrDetail( policeNr: string, jahr: string ) {
+        let url = environment.production ? this.pl.getBaseHrefFromDOM() + this._crDetailUrlProd : this._crDetailUrlDev;
+        console.log( url );
+        if ( environment.production ) {
+            let start = url.indexOf( "/crdetail/" );
+            console.log( "start=" + start );
+            if ( start > -1 ) {
+                let end = url.indexOf( "/rest/" );
+                url = url.substring( 0, start ) + url.substring( end, url.length );
+            } else {
+                url = this._crDetailUrlProd.substring( 1 );
+                console.log( url );
+            }
+        }
+        url = url.replace( "{mietNr}", policeNr ).replace( "{jahr}", jahr );
+        return this.http.get( url, this._reqOptionsArgs ).map( res => this.unpackDetailResponse( res ) ).catch( this.handleError );
+    }
+
+    private unpackTableResponse( res: Response ): CrTableRow[] {
+        //console.log(res);     
+        return <CrTableRow[]>res.json();
+    }
+
+    private unpackDetailResponse( res: Response ): CrDetail {
+        //console.log(res);     
+        return <CrDetail>res.json();
+    }
+
+    private handleError( error: Response ) {
+        // in a real world app, we may send the error to some remote logging infrastructure
+        // instead of just logging it to the console
+        console.error( JSON.stringify( error ) );
+        return Observable.throw( error );
+    }
 }
