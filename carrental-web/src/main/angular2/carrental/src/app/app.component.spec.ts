caf9a48f33a287a520b82cfc5bbff8c9bf6ecb84
Your Name <you@example.com>
2017-01-22 12:06:43 +0100

    Angular-Cli update

--- carrental-web/src/main/angular2/carrental/src/app/app.component.spec.ts
+++ carrental-web/src/main/angular2/carrental/src/app/app.component.spec.ts
@@ -13,20 +13,37 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from '../app/app.component';
+/* tslint:disable:no-unused-variable */
 
-//beforeEachProviders(() => [LsdeklAppComponent]);
-//
-//describe('App: Lsdekl', () => {
-//  it('should create the app',
-//      inject([LsdeklAppComponent], (app: LsdeklAppComponent) => {
-//    expect(app).toBeTruthy();
-//  }));
-//
-//  it('should have as title \'lsdekl works!\'',
-//      inject([LsdeklAppComponent], (app: LsdeklAppComponent) => {
-//    expect(app.title).toEqual('lsdekl works!');
-//  }));
-//});
- 
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    });
+    TestBed.compileComponents();
+  });
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  it(`should have as title 'app works!'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app works!');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('carrental works!');
+  }));
+});
