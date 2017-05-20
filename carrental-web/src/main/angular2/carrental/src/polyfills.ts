f00c2b20b2fbbcc0a08d8a410c0343274f14e402
Your Name <you@example.com>
2017-05-20 23:35:47 +0200

    add polyfill for input type="date"

--- carrental-web/src/main/angular2/carrental/src/polyfills.ts
+++ carrental-web/src/main/angular2/carrental/src/polyfills.ts
@@ -82,3 +82,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 import 'intl';  // Run `npm install --save intl`.
+
+// for input type="date"
+import 'html5-simple-date-input-polyfill';
