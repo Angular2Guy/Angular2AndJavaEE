52df4d2ebb3de11d280465cb900bb22d13693492
Your Name <you@example.com>
2017-05-30 01:10:05 +0200

    Add Pdf download to Rest Interface

--- carrental-web/src/main/java/ch/xxx/carrental/ui/rest/model/CrTableResource.java
+++ carrental-web/src/main/java/ch/xxx/carrental/ui/rest/model/CrTableResource.java
@@ -1,17 +1,21 @@
 package ch.xxx.carrental.ui.rest.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Locale;
 
 import javax.inject.Inject;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.OPTIONS;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.apache.commons.io.IOUtils;
 
 import ch.xxx.carrental.ui.interceptor.DisableCaching;
 import ch.xxx.carrental.ui.service.CrTableService;
@@ -51,5 +55,20 @@ public class CrTableResource {
 			return Response.ok().build();
 		}
 	}
-		
+	
+	@GET
+	@Path("/pdf")
+	@Produces("application/pdf")
+	@DisableCaching
+	public Response getPdf(@PathParam("mietNr") final String mietNr, @HeaderParam("Origin") final String origin, @HeaderParam("Accept-Language") final String acceptLang) {
+		byte[] array = null;
+		try {			
+			array = IOUtils.toByteArray(this.getClass().getResourceAsStream("../../pdf/BigCalendar.pdf"));			
+		} catch (IOException e) {
+			return Response.serverError().entity("Failed to read File.").build();
+		}
+		return Response.ok(array).header("Content-Disposition",
+				"attachment; filename=BigCalendar.pdf").build();
+	}
+	
 }
