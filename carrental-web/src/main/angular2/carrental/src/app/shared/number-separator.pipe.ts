a55b8e6feba49ec7e57093ecfed7e5c697724ade
Your Name <you@example.com>
2017-06-11 08:48:09 +0200

    add Pipe to display component

--- carrental-web/src/main/angular2/carrental/src/app/shared/number-separator.pipe.ts
+++ carrental-web/src/main/angular2/carrental/src/app/shared/number-separator.pipe.ts
@@ -5,19 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NumberSeparatorPipe implements PipeTransform {
 
-  transform(value: string, args?: string): string {
-      if(!isNaN(+value) && !isNaN(+args[0])) {          
-          let digits = Number(args);
+  transform(value: string, args?: string): string {      
+      if(!isNaN(parseInt(value)) && !isNaN(parseInt(args))) {          
+          let digits = parseInt(args);
           let arr = [];                                 
-          while(value.length > digits) {
-              let str = value.slice(value.length-digits, value.length);
+          let myValue = String(value);
+          while(myValue.length > digits) {
+              let str = myValue.slice(myValue.length-digits, myValue.length);
               arr.push("'"+str);
-              value = value.slice(0,value.length-digits);
+              myValue = myValue.slice(0,myValue.length-digits);
           }           
           arr.reverse();
-          let result = "" + value +arr;          
-          return result.replace(",","");
+          let result = "" + myValue +arr;          
+          result = result.replace(",","");
+//          console.log("result1 "+result);
+          return result;
       }          
+//      console.log("result2 "+value);
       return value;
   }
 
