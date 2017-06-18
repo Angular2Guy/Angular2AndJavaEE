5765a61ab10cfc858746dc11067bc3e7a2a8c0cb
Your Name <you@example.com>
2017-06-18 13:16:48 +0200

    add Protractor Test

--- carrental-web/src/main/angular2/carrental/e2e/app.e2e-spec.ts
+++ carrental-web/src/main/angular2/carrental/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { TestprojectPage } from './app.po';
+import { browser, by, element, By, $, $$, ExpectedConditions } from 'protractor';
 
 describe('testproject App', () => {
   let page: TestprojectPage;
@@ -13,4 +14,16 @@ describe('testproject App', () => {
       .then(msg => expect(msg).toEqual('carrental works!'))
       .then(done, done.fail);
   });
+  it('should display the list page', done => {
+      page.navigateToList();
+      page.getParagraphTextList()
+          .then(msg => expect(msg).toEqual('crlist works!'))
+          .then(done, done.fail);
+  });
+//  it('should display 2 lines in the table', done => {
+//      
+//      page.getListTableLength()
+//         .then(length => expect(length).toEqual(2))
+//         .then(done, done.fail);
+//  });
 });
--- carrental-web/src/main/angular2/carrental/e2e/app.po.ts
+++ carrental-web/src/main/angular2/carrental/e2e/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, By, $, $$, ExpectedConditions } from 'protractor';
 
 export class TestprojectPage {
   navigateTo() {
@@ -7,5 +7,20 @@ export class TestprojectPage {
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+  
+  navigateToList() {
+      return element(by.id('myList')).click();      
+  }
+  
+  getParagraphTextList() {
+      return element(by.id('myListId')).getText();
+  }
+  
+  async getListTableLength() {
+      let table = element.all(by.repeater('let row of tableRows | async'));
+      if((await table).length > 0) {          
+          return (await table).length;
+      }      
   }
 }
