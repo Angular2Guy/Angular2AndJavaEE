c0bcabf9f312155b6502ef2cf13d81e0839e8213
sven1 <sven1@falcon>
2018-05-14 23:20:28 +0200

    update tests

--- carrental-web/src/main/angular/carrental/src/karma.conf.js
+++ carrental-web/src/main/angular/carrental/src/karma.conf.js
@@ -24,9 +24,13 @@ module.exports = function (config) {
     angularCli: {      
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+//  reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+    	outputDir: 'reports', // results will be saved as $outputDir/$browserName.xml
+        outputFile: 'junit.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+        useBrowserName: false // add browser name to report and classes names
+          },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
