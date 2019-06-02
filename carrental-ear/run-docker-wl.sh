18ce90f5fa90325604d771dbea2a92d9c49eda20
Angular2Guy <angular2guy@gmail.com>
2019-06-02 10:31:27 +0200

    update Docker scripts

--- carrental-ear/run-docker-wl.sh
+++ carrental-ear/run-docker-wl.sh
@@ -1,2 +1,2 @@
-docker build -t angular2guy:carrental-wl -f Dockerfile-wl .
-docker run -p 8001:8001 angular2guy:carrental-wl
+docker build -t angular2guy/angular2andjavaee-wl -f Dockerfile-wl .
+docker run -p 8001:8001 angular2guy/angular2andjavaee-wl
