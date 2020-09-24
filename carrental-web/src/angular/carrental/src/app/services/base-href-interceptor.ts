33be7d136c528f396b257e348995b7be4f1e5dcd
Angular2Guy <angular2guy@gmail.com>
2020-09-25 00:24:07 +0200

    make Interceptor run

--- carrental-web/src/angular/carrental/src/app/services/base-href-interceptor.ts
+++ carrental-web/src/angular/carrental/src/app/services/base-href-interceptor.ts
@@ -1,4 +1,19 @@
-import { Injectable, Inject } from '@angular/core';
+/**
+ *    Copyright 2016 Sven Loesekann
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, of} from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -15,11 +30,12 @@ export class BaseHrefInterceptor implements HttpInterceptor {
 	 	private crRestService: CrRestService) {}
 	
  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+	const myBaseHref = !this.platformLocation.getBaseHrefFromDOM() || this.platformLocation.getBaseHrefFromDOM().length < 2 ? '//'.split('/') : this.platformLocation.getBaseHrefFromDOM().split('/');
 	req = req.clone({
 		headers: this.defaultReqHeader,
-		url: (!this.platformLocation.getBaseHrefFromDOM() || this.platformLocation.getBaseHrefFromDOM().length < 2 ? '' : this.platformLocation.getBaseHrefFromDOM()) + req.url, 
+		url: (myBaseHref[1].length > 0 ? '/'+myBaseHref[1] : '') + req.url, 
 	});	
-	console.log(this.platformLocation.getBaseHrefFromDOM());
+	//console.log(myBaseHref);
   	return next.handle(req).pipe(tap(event => event, event => this.handleError(event)));
   }
 
@@ -27,7 +43,9 @@ export class BaseHrefInterceptor implements HttpInterceptor {
 	if(event instanceof HttpErrorResponse) {
 		const error = <HttpErrorResponse> event;
 		console.log(`failed: ${error.message}`);
-		this.crRestService.putCrLogMsg(new CrLogMsgImpl('Error', error.message)).subscribe();
+		if(error.url.indexOf('crLog') < 0) {
+			this.crRestService.putCrLogMsg(new CrLogMsgImpl('Error', error.message)).subscribe();
+		}
 	}
 	return event;
   }	
