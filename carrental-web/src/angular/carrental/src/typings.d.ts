5d78670c4207aeee98540c2eea5a479bb104decd
Angular2Guy <angular2guy@gmail.com>
2020-11-15 10:28:00 +0100

    fix

--- carrental-web/src/angular/carrental/src/typings.d.ts
+++ carrental-web/src/angular/carrental/src/typings.d.ts
@@ -1,5 +1,5 @@
 /* SystemJS module definition */
-declare let module: NodeModule;
-interface NodeModule {
-  id: string;
-}
+//declare let module: NodeModule;
+//interface NodeModule {
+//  id: string;
+//}
