635b2a2de8d2c917570c04d024f96edf72f798a0
Angular2Guy <angular2guy@gmail.com>
2022-04-09 15:55:15 +0200

    fix: tests

--- carrental-web/src/test/java/ch/xxx/carrrental/ui/rest/model/CrTableResourceTest.java
+++ carrental-web/src/test/java/ch/xxx/carrrental/ui/rest/model/CrTableResourceTest.java
@@ -1,8 +1,38 @@
+/**
+ *    Copyright 2016 Sven Loesekann
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package ch.xxx.carrrental.ui.rest.model;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ch.xxx.carrental.ui.dto.CrTableRow;
+import ch.xxx.carrental.ui.rest.model.CrTableResource;
+import ch.xxx.carrental.ui.service.CrTableService;
 
 public class CrTableResourceTest {
-/*	
 	CrTableService service;
 	
 	@Before
@@ -27,5 +57,4 @@ public class CrTableResourceTest {
 		Response resp = testee.getAll("1", "en");		
 		Assert.assertEquals(200, resp.getStatus());
 	}
-	*/
 }
