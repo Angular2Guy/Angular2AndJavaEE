916459a70fe1021e1be12e60d59fd574cbe00337
Angular2Guy <angular2guy@gmail.com>
2022-10-02 16:19:21 +0200

    fix: swagger

--- carrental-web/src/main/webapp/swagger/swagger-initializer.js
+++ carrental-web/src/main/webapp/swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:8080/carrental-web/rest/swagger.json",
+    url: `${window.location.href.split('/swagger')[0]}/rest/swagger.json`,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
